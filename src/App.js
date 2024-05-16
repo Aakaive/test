a6ae9b1d5a72a3f85d80ad823fe952c727622249
Aakaive <wonseok031@gmail.com>
2024-05-16 17:20:00 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.aaaa
+          Edit <code>src/App.js</code> 테스트용 텍스트 입력.
         </p>
         <a
           className="App-link"
